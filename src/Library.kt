aba560373bb248cc0b5893dd3748a8c515370bd5
blankek <kombarus05@gmail.com>
2025-09-09 15:57:32 +0500

    full version

--- src/Library.kt
+++ src/Library.kt
@@ -17,8 +17,8 @@ class Library {
         println(bookName + " добавлена")
     }
 
-    fun addReader(readerName: String?){
-        val readerName = readerName?.trim()
+    fun addReader(readerNameInput: String?){
+        val readerName = readerNameInput?.trim()
         if(readerName.isNullOrEmpty()){
             println("Введите автора")
             return
@@ -27,32 +27,73 @@ class Library {
         println(readerName + " добавлен")
     }
 
-    fun findBook(bookName: String?){
-        val query = bookName?.trim()
-        if(query.isNullOrEmpty()){
+    fun findBook(bookNameInput: String?){
+        val bookName = bookNameInput?.trim()
+        if(bookName.isNullOrEmpty()){
             println("Введите название книги")
             return
         }
-        val result = booksList.filter{it.name.contains(query, ignoreCase = true)}
+        val result = booksList.filter{it.name.contains(bookName, ignoreCase = true)}
         if(result.isEmpty()){ println("Ничего не найдено"); return}
-        for (book in booksList){
-            val status = if(borrowed.containsKey(book.id)){
-                val readerId = borrowed[book.id]
-                val readerName = readersList.find{it.id == readerId}?.name ?: "неизвестен"
-                "выдана читателю $readerName id=$readerId"
-            }else{
-                "в наличии"
-            }
-            println("[${book.id}] ${book.name} - $status")
+        println("$bookName найдена")
+    }
+
+    fun giveBook(bookIdInput: String?, readerIdInput: String?){
+        val bookId = bookIdInput?.toIntOrNull()
+        val readerId = readerIdInput?.toIntOrNull()
+
+        if(bookId == null) { println("Неверный Id книги"); return}
+        if(readerId == null) { println("Неверный Id читателя"); return}
+
+        val bookName = booksList.find{ it.id == bookId}
+        val readerName = readersList.find { it.id == readerId }
+
+        if(bookName == null){ println("Книга не найдена"); return}
+        if(readerName == null){ println("Читатель не найден"); return}
+
+        if(borrowed.containsKey(bookId)){
+            println("Книга уже выдана")
+            return
         }
+
+        borrowed[bookId] = readerId
+        println("Книга ${bookName.name} выдана читателю ${readerName.name}")
+    }
+
+    fun returnBook(bookIdInput: String?){
+        val bookId = bookIdInput?.toIntOrNull()
+        if (bookId == null) { println("Неверный Id книги"); return}
+        if (!borrowed.containsKey(bookId)){
+            println("Эта книга не выдана")
+            return
+        }
+
+        borrowed.remove(bookId)
+        val name = booksList.find{ it.id == bookId }?. name ?: "неизвестна"
+        println("книга $name возвращена в библиотеку")
     }
 
     fun showAllBooks(){
-        if (booksList.isNullOrEmpty()){
-            println("Библиотека пуста")
+        if (booksList.isEmpty()) { println("библиотека пуста"); return}
+        println("Все книги")
+        for(book in booksList){
+            val status = if(borrowed.containsKey(book.id)){
+                val readerId = borrowed[book.id]
+                val readerName = readersList.find { it.id == readerId }?.name?:"неизвестен"
+                "выдана $readerName"
+            }else{
+                "В наличии"
+            }
+            println(" [${book.id}] ${book.name} - $status")
         }
-        println("Все книги: ")
-        println(booksList.joinToString())
+    }
+
+    fun showAllReader(){
+        if (readersList.isEmpty()){ println("Читателей нет"); return}
+        println("Читатели")
+        for (reader in readersList){
+            println(" [${reader.id}] ${reader.name}")
+        }
     }
 
 }
--- src/Main.kt
+++ src/Main.kt
@@ -6,7 +6,10 @@ fun main() {
             1) Добавить книгу
             2) Добавить автора
             3) Найти книгу
+            4) Взять книгу
+            5) Вернуть книгу
             6) Показать все книги
+            7) Показать всех читателей
             0) Выход
         """.trimIndent())
         when(readLine()?.trim()){
@@ -15,15 +18,30 @@ fun main() {
                 library.addBook(readLine())
             }
             "2" -> {
-                println("Введите имя автора")
+                println("Введите имя читателя")
                 library.addReader(readLine())
             }
-            "3" ->{
+            "3" -> {
                 println("Введите название книги")
                 library.findBook(readLine())
             }
+            "4" -> {
+                println("Введите Id книги для выдачи")
+                val bookId = readLine()
+                println("Введите Id читателя для выдачи")
+                val readerId = readLine()
+                library.giveBook(bookId, readerId)
+            }
+            "5" -> {
+                println("Введите id книги для возврата")
+                val bookId = readLine()
+                library.returnBook(bookId)
+            }
             "6" -> {
                 library.showAllBooks()
+            }
+            "7" -> {
+                library.showAllReader()
             }
             "0" -> {
                 return
